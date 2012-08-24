8e4fe2dbd2aeb28b092686a7aa3242805690b769
raptor <raptor.zh@gmail.com>
2012-08-24 22:42:53 +0800

    a completely new version

--- gcrawler.py
+++ gcrawler.py
@@ -3,7 +3,7 @@
     gevent crawler
     ~~~~~~~~~~~~~~~~
     BSD License
-    2011 by raptor.zh@gmail.com.
+    2011-12 by raptor.zh@gmail.com.
 """
 import gevent
 from gevent import monkey, queue
@@ -11,67 +11,10 @@ from gevent import monkey, queue
 monkey.patch_all()
 
 import urllib2
-from time import sleep
-import traceback
 import logging
 
 logger = logging.getLogger(__name__)
 
-class GCrawler:
-    def __init__(self, spider, timeout=5, workers_count=8, pipeline_size=100):
-        self.spider = spider
-        self.timeout = timeout
-        self.qin = queue.Queue(0)
-        self.qout = queue.Queue(pipeline_size)
-        self.jobs = [gevent.spawn(self.doScheduler)]
-        self.jobs += [gevent.spawn(self.doWorker) for i in range(workers_count)]
-        self.jobs += [gevent.spawn(self.doPipeline)]
-        self.job_count = len(self.jobs)
-
-    def start(self):
-        gevent.joinall(self.jobs)
-
-    def doScheduler(self):
-        try:
-            scheduler = self.spider.scheduler()  #  return a generator
-            for item in scheduler:
-                self.qin.put(item)
-        except Exception, e:
-            logger.error("Scheduler Error!\n%s" % traceback.format_exc())
-        finally:
-            for i in range(self.job_count - 2):
-                self.qin.put(StopIteration)
-            self.job_count -= 1
-            logger.debug("Scheduler done, job count: %s" % self.job_count)
-
-    def doWorker(self):
-        try:
-            item = self.qin.get()
-            while item != StopIteration:
-                try:
-                    r = self.spider.worker(item)
-                    if r != None:
-                        self.qout.put(r)
-                except:
-                    logger.error("Worker error!\n%s" % traceback.format_exc())
-                item = self.qin.get()
-        finally:
-            self.job_count -= 1
-            logger.debug("Worker done, job count: %s" % self.job_count)
-
-    def doPipeline(self):
-        while self.job_count > 1 or not self.qout.empty():
-            sleep(self.timeout)
-            try:
-                results = []
-                try:
-                    while True:
-                        results.append(self.qout.get_nowait())
-                except queue.Empty:
-                    if results != []:
-                        self.spider.pipeline(results)
-            except:
-                logger.error("Pipeline error!\n%s" % traceback.format_exc()) 
 
 def retryOnURLError(trycnt=3):
     def funcwrapper(fn):
@@ -80,57 +23,79 @@ def retryOnURLError(trycnt=3):
                 try:
                     return fn(self, *args, **kwargs)
                 except urllib2.URLError, e:
-                    logger.info("retry %s time(s)" % (i+1))
+                    logger.info("retry %s time(s)" % (i + 1))
                     if i == trycnt - 1:
                         raise e
         return wrapper
     return funcwrapper
 
-class Downloader:
-    def __init__(self, urls):
-        self.urls = urls
-        self.subitems = queue.Queue(-1)
-        self.parsings = 0
 
-    def scheduler(self):
-        parse=None
-        if hasattr(self, 'parse'):
-            parse = self.parse
-        for u in self.urls:
-            if parse != None:
-                self.parsings += 1
-            yield dict(url=u, parse=parse)
-        while self.parsings > 0:
-            sleep(1)
-            while not self.subitems.empty():
-                item = self.subitems.get()
-                try:
-                    if item['parse'] != None:
-                        self.parsings += 1
-                except:
-                    pass
-                yield item
+class Request:
+    def __init__(self, url="", parser=None):
+        self.url    = url
+        self.parser = parser
 
-    def worker(self, item):
-        r = None
-        try:
-            r = self.doWorker(item['url'])
-            if r != None and item['parse'] != None:
-                try:
-                    new_items, r = item['parse'](r)
-                    for i in new_items:
-                        self.subitems.put(i)
-                finally:
-                    self.parsings -= 1
-        except Exception, e:
-            logger.error("Error on get %s:%s\n%s" % (item['url'], e, traceback.format_exc()))
-        return r
 
-    @retryOnURLError(3)
-    def doWorker(self, url):
-        logger.debug("Download starting...%s" % url)
+class Response:
+    def __init__(self, request=None, result=None):
+        self.request = request
+        self.result  = result
+
+
+class Scheduler:
+    def __init__(self, urls, parser, pipeline, max_running=20):
+        self.pipeline = pipeline
+        self.pendings = queue.Queue(-1)
+        self.responses = queue.Queue(-1)
+        self.max_running = max_running
+        for url in urls:
+            self.pendings.put(Request(url=url, parser=parser))
+        gevent.spawn(self.doSchedule).join()
+
+    @retryOnURLError()
+    def fetch(self, url):
         f = urllib2.urlopen(url)
         data = f.read()
         f.close()
-        return dict(url=url, data=data)
+        return data
 
+    def parser(self, req):
+        results = []
+        try:
+            data = self.fetch(req.url)
+            if req.parser != None:
+                res = req.parser(req.url, data)
+                for r in res:
+                    if type(r) == type(Request()) and r.__class__ == Request:
+                        self.pendings.put(r)
+                    else:
+                        results.append(r)
+            else:
+                results.append(data)
+        except:
+            import traceback
+            traceback.print_exc()
+            results = None
+        self.responses.put(Response(request=req, result=results))
+
+    def doSchedule(self):
+        runnings = 0
+        while runnings > 0 or not self.pendings.empty():
+            try:
+                while runnings < self.max_running:
+                    req = self.pendings.get_nowait()
+                    logger.debug("new greenlet for: %s" % req.url)
+                    gevent.spawn(self.parser, req)
+                    runnings += 1
+            except queue.Empty:
+                pass
+            resp = self.responses.get()
+            runnings -= 1
+            if resp.result == None:  # None means error!
+                logger.error("Fetch fail: %s" % resp.request.url)
+            elif resp.result != []:
+                try:
+                    self.pipeline(resp)
+                except:
+                    import traceback
+                    traceback.print_exc()
--- testgcrawler.py
+++ testgcrawler.py
@@ -3,9 +3,9 @@
     test gcrawler
     ~~~~~~~~~~~~~~~~
     BSD License.
-    2011 by raptor.zh@gmail.com.
+    2011-12 by raptor.zh@gmail.com.
 """
-from gcrawler import GCrawler, Downloader
+from gcrawler import Scheduler
 import unittest
 import urllib2
 import logging
@@ -15,67 +15,26 @@ import re
 
 logging.basicConfig(level=logging.DEBUG)
 
-urls=['http://www.163.com', 'http://www.qq.com', 'http://www.sina.com.cn', 'http://www.sohu.com', 'http://www.yahoo.com', 'http://www.baidu.com', 'http://www.google.com', 'http://www.microsoft.com']
+urls = ['http://www.163.com', 'http://www.qq.com', 'http://www.sina.com.cn',
+        'http://www.sohu.com', 'http://www.yahoo.com', 'http://www.baidu.com',
+        'http://www.apple.com', 'http://www.microsoft.com']
 
-class DummySpider:
-    def scheduler(self):
-        for u in urls:
-            yield u
 
-    def worker(self, item):
-        try:
-            f = urllib2.urlopen(item)
-            data = f.read()
-            f.close()
-            return (item, len(data))
-        except:
-            traceback.print_exc()
-            return None
+class Crawler:
+    def parser(self, req_url, data):
+        return [len(data)]
 
-    def pipeline(self, results):
-        for r in results:
-            print "Downloaded : %s(%s)" % r
+    def pipeline(self, response):
+        url = response.request.url
+        print "Data fetched: %s %s" % (url, response.result[0])
 
-class DummyDownloader(Downloader):
-    def pipeline(self, results):
-        for r in results:
-            r['datasize'] = len(r['data'])
-            print "Data fetched : %(url)s(%(datasize)s)" % r
-
-class ImagesDownloader(Downloader):
-    def parse(self, result):
-        pat_imgs = re.compile(u"<img.*src=\"([^\"]*)", re.I | re.U)
-        imgs = list(set(pat_imgs.findall(result['data'])))
-        print "Images count %s" % len(imgs)
-        new_items = []
-        for i in imgs:
-            if i != "":
-                item = dict(url=i, parse=None)
-                new_items.append(item)
-        return new_items, result
-
-    def pipeline(self, results):
-        print "== %s" % datetime.now()
-        for r in results:
-            r['datasize'] = len(r['data'])
-            print "Data fetched : %(url)s(%(datasize)s)" % r
 
 class TestGCrawler(unittest.TestCase):
-    def testSpider(self):
-        spider = DummySpider()
-        crawler = GCrawler(spider, workers_count = 3)
-        crawler.start()
+    def testCrawler(self):
+        dt = datetime.now()
+        crawler = Crawler()
+        Scheduler(urls, crawler.parser, crawler.pipeline, 8)
+        print datetime.now() - dt
 
-    def testDownloader(self):
-        spider = DummyDownloader(urls)
-        crawler = GCrawler(spider, workers_count = 3)
-        crawler.start()
-
-    def testBatch(self):
-        print "start at : %s" % datetime.now()
-        spider = ImagesDownloader(['http://f1.163.com'])
-        crawler = GCrawler(spider, workers_count = 8)
-        crawler.start()
 
 unittest.main()
-
